4cb8cd32b22c4439b93bcbc7d8b5a988fc3b480b
Lãng <jhsh@mail.ccsf.edu>
2019-04-18 21:40:42 +0700

    Done add, update category product

--- WebShop.Web/Models/ProductCategoryViewModel.cs
+++ WebShop.Web/Models/ProductCategoryViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,17 @@ namespace WebShop.Web.Models
     public class ProductCategoryViewModel
     {
         public int ID { set; get; }
+
+        [Required]
         public string Name { set; get; }
 
-
+        [Required]
         public string Alias { set; get; }
+
         public string Description { set; get; }
 
         public int? ParentID { set; get; }
+
         public int? DisplayOrder { set; get; }
 
         public string Image { set; get; }
@@ -25,19 +30,17 @@ namespace WebShop.Web.Models
 
         public DateTime? CreatedDate { set; get; }
 
-
         public string CreatedBy { set; get; }
 
         public DateTime? UpdatedDate { set; get; }
 
-
         public string UpdatedBy { set; get; }
-
 
         public string MetaKeyword { set; get; }
 
         public string MetaDescription { set; get; }
 
+        [Required]
         public bool Status { set; get; }
     }
 }
